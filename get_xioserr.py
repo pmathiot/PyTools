6b431f29d2eb12d6057fce5aa3b85b0269f95670
Pierre Mathiot <pierre.mathiot@metoffice.gov.uk>
2017-09-08 12:06:08 +0100

    add cat err

--- get_xioserr.py
+++ get_xioserr.py
@@ -1,10 +1,8 @@
 #!/bin/python
-from netCDF4 import Dataset
-import numpy as np
-import sys, re
+# author: Pierre mathiot (09/2017)
+
 import glob, os
 import argparse
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s" , metavar='suffix_name', help="names of suffix file name (xios error file)", type=str, nargs=1, required=True)
@@ -13,19 +11,24 @@ args = parser.parse_args()
 csuf = args.s[0]
 
 # sort list
-file_lst=glob.glob(csuf+"_????.nc")
+file_lst=glob.glob(csuf+"_????.err")
 file_lst.sort()
 
 nfind=0
 for filename in file_lst:
-    print filename, os.stat(filename).st_size
-    if (os.stat(filename).st_size /= 0)
+    if (os.stat(filename).st_size != 0):
         print ''
         print ' ========================================================'
-        print ' file name : filename contain an error'.format(filename) 
+        print ' file name : {} contain an error'.format(filename) 
         print ' ========================================================'
         print ''
-    nfind=nfind+1
+        with open(filename, 'rb') as file:
+            for line in file:
+                print line,
+
+        nfind=nfind+1
     
-if (nfind == 0L):
+if (nfind == 0):
+    print ''
     print ' There no error in the xios error file '+csuf+'_????.nc'
+    print ''
